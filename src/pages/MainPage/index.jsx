29039c5eef42f3f0953ea0d83e236b462f964a12
Taewan Lim <taewan.seoul@gmail.com>
2022-07-05 21:03:01 +0900

    ✨ feat: set up MainPage with form inputs

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,5 +1,74 @@
+import { useRecoilState } from "recoil";
+
+import styled from "styled-components";
+
+import birthdayState from "../../lib/recoil/user";
+import useInputs from "../../lib/hooks/useInputs";
+import calculateBirthday from "../../lib/utils/calculateBirthday";
+
 function MainPage() {
-  return <></>;
+  const [birthday, setBirthday] = useRecoilState(birthdayState);
+  const { year, month, day } = birthday;
+  const [{ birthYear, birthMonth, birthDay }, onChange, reset] = useInputs({
+    birthYear: "",
+    birthMonth: "",
+    birthDay: "",
+  });
+
+  const handleWheelChange = e => calculateBirthday(e, birthday, setBirthday);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    onChange({ target: { name: "birthYear", value: year } });
+    onChange({ target: { name: "birthMonth", value: month } });
+    onChange({ target: { name: "birthDay", value: day } });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Wrapper>
+          <Heading>When did you start your life?</Heading>
+          <InputWrapper>
+            <input
+              type="number"
+              name="year"
+              value={year}
+              onWheel={handleWheelChange}
+            />
+            <input
+              type="number"
+              name="month"
+              value={month}
+              onWheel={handleWheelChange}
+            />
+            <input
+              type="number"
+              name="day"
+              value={day}
+              onWheel={handleWheelChange}
+            />
+          </InputWrapper>
+          <button type="submit">Login</button>
+        </Wrapper>
+      </form>
+    </>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 60px);
+`;
+
+const InputWrapper = styled.div``;
+
+const Heading = styled.div`
+  margin: 30px;
+`;
 
 export default MainPage;
