8e3c57095d9c2f4ca56ec45780c0c6ddd279f3c0
Taewan Lim <taewan.seoul@gmail.com>
2022-07-06 04:50:51 +0900

    ✨ feat: set up login atom and modal

--- src/components/GlobalModal/index.jsx
+++ src/components/GlobalModal/index.jsx
@@ -3,13 +3,18 @@ import { useRecoilValue } from "recoil";
 import modalState from "../../lib/recoil/modal";
 
 import Modal from "../Modal";
+import Login from "../Login";
 
 function GlobalModal() {
   const { modalType, modalProps } = useRecoilValue(modalState) || {};
 
   switch (modalType) {
     case "LoginModal":
-      return <Modal></Modal>;
+      return (
+        <Modal>
+          <Login />
+        </Modal>
+      );
   }
 }
 
