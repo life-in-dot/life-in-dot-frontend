200b96cca03ad96767b75826be6025b3cfb657a8
Taewan Lim <taewan.seoul@gmail.com>
2022-07-10 03:34:18 +0900

    ♻️ refactor: refactor D3 LifeDot and util function

--- src/components/LifeDot/index.jsx
+++ src/components/LifeDot/index.jsx
@@ -1,23 +1,17 @@
 import { useEffect, useRef } from "react";
-import * as d3 from "d3";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
+import * as d3 from "d3";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-function LifeDot({ data, yearData }) {
+import { yearsListState } from "../../lib/recoil/user";
+
+function LifeDot() {
+  const navigate = useNavigate();
   const svgRef = useRef();
-
-  const userYearData = [];
-
-  for (let i = 1; i < 11; i++) {
-    for (let j = 1; j < 11; j++) {
-      userYearData.push({
-        r: 0.1,
-        x: i,
-        y: j,
-      });
-    }
-  }
+  const hundredYears = useRecoilValue(yearsListState);
 
   useEffect(() => {
     const width = window.innerWidth / 2;
@@ -25,33 +19,46 @@ function LifeDot({ data, yearData }) {
 
     const svg = d3
       .select(svgRef.current)
+      .append("svg")
+      .attr("class", "life-board")
       .attr("viewBox", [0, 0, width, height]);
 
-    const gLife = svg.append("g");
+    const gLife = svg.append("g", "life-board").attr("class", "life-dot");
     gLife
       .selectAll("circle")
-      .data(data)
+      .data([10])
       .join("circle")
-      .attr("r", d => d / 2)
-      .attr("cx", (d, i) => width / 2 + 2 * i)
+      .attr("r", d => d)
+      .attr("cx", (d, i) => width / 2)
       .attr("cy", (d, i) => height / 2)
       .attr("fill", "#22BC5E")
       .attr("stroke", "#BDE5EC")
       .attr("stroke-width", 3)
       .attr("opacity", 0.3);
 
-    const gYear = svg.append("g");
+    const gYear = svg.append("g", "life-board").attr("class", "year-dots");
     gYear
       .selectAll("circle")
-      .data(userYearData)
+      .data(hundredYears)
       .join("circle")
       .attr("r", d => d.r / 10)
-      .attr("cx", (d, i) => width / 2 + d.x / 10)
-      .attr("cy", (d, i) => height / 2 + d.y / 10)
+      .attr("cx", (d, i) => width / 2 + d.y / 10)
+      .attr("cy", (d, i) => height / 2 + d.x / 10)
+      .attr("class", d => `${d.year}`)
       .attr("fill", "#22BC5E")
       .attr("stroke", "#BDE5EC")
       .attr("stroke-width", 0.003)
-      .attr("opacity", 0.5);
+      .attr("opacity", 0.5)
+      .on("wheel", event => {
+        const zoomScale = svg._groups[0][0].__zoom.k;
+
+        if (zoomScale > 50000) {
+          event.target.style.transition = "opacity 1s ease-out";
+          event.target.style.opacity = "0";
+
+          navigate("/year", { replace: false });
+        }
+      });
 
     const zoomed = ({ transform }) => {
       gLife.attr("transform", transform);
@@ -78,7 +85,7 @@ LifeDot.propTypes = {
   yearData: PropTypes.array,
 };
 
-const Main = styled.svg`
+const Main = styled.div`
   background-color: rgba(189, 229, 236, 0.3);
   height: 100%;
   width: 100%;
