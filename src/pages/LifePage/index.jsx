774a936ce85cb40b7db6f5c591943594a9447245
Taewan Lim <taewan.seoul@gmail.com>
2022-07-10 03:31:43 +0900

    ✨ feat: set up LifePage and YearPage

--- src/pages/LifePage/index.jsx
+++ src/pages/LifePage/index.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-
 import styled from "styled-components";
 
 import LifeDot from "../../components/LifeDot";
 
 function LifePage() {
-  const [data, setData] = useState([30]);
-  const [yearData, setYearData] = useState([]);
-
   return (
     <Wrapper>
-      <LifeDot data={data} yearData={yearData}></LifeDot>
+      <LifeDot></LifeDot>
     </Wrapper>
   );
 }
@@ -20,8 +15,7 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 70px;
-  height: calc(100vh - 70px);
+  height: 100vh;
   width: 100vw;
 `;
 
