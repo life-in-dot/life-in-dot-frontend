df267c725f26e865aca4a4732b7eb7d9c6aafe64
Taewan Lim <taewan.seoul@gmail.com>
2022-07-21 18:55:30 +0900

    💄 style: add style on login page

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,6 +1,7 @@
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 import styled from "styled-components";
+import { CgScrollV } from "react-icons/cg";
 
 import birthdayState, {
   hundredyearsListState,
@@ -47,33 +48,48 @@ function MainPage() {
     <>
       <form onSubmit={handleSubmit}>
         <Wrapper>
-          <Heading>When did you start your life?</Heading>
+          <Heading>
+            <p>When is your birthday?</p>
+            <p>Scroll on numbers.</p>
+          </Heading>
           <InputWrapper>
-            <input
-              type="number"
-              name="year"
-              value={birthYear}
-              onWheel={handleWheelChange}
-              readOnly={true}
-            />
-            <input
-              type="number"
-              name="month"
-              value={birthMonth}
-              onWheel={handleWheelChange}
-              readOnly={true}
-            />
-            <input
-              type="number"
-              name="date"
-              value={birthDate}
-              onWheel={handleWheelChange}
-              readOnly={true}
-            />
+            <InputFieldDiv>
+              <InputField
+                type="number"
+                name="year"
+                value={birthYear}
+                onWheel={handleWheelChange}
+                onTouchMove={handleWheelChange}
+                readOnly={true}
+              />
+              <ScrollIcon />
+            </InputFieldDiv>
+            <InputFieldDiv>
+              <InputField
+                type="number"
+                name="month"
+                value={birthMonth}
+                onWheel={handleWheelChange}
+                onTouchMove={handleWheelChange}
+                readOnly={true}
+              />
+              <ScrollIcon />
+            </InputFieldDiv>
+            <InputFieldDiv>
+              <InputField
+                type="number"
+                name="date"
+                value={birthDate}
+                onWheel={handleWheelChange}
+                onTouchMove={handleWheelChange}
+                readOnly={true}
+              />
+              <ScrollIcon />
+            </InputFieldDiv>
           </InputWrapper>
-          <button type="submit" onClick={handleLoginClick}>
+          <LoginButton type="submit" onClick={handleLoginClick}>
             Login
-          </button>
+          </LoginButton>
         </Wrapper>
       </form>
     </>
@@ -81,18 +97,123 @@ function MainPage() {
 }
 
 const Wrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  @media (max-width: 780px) {
+    width: 300px;
+  }
+  width: 500px;
+  height: 350px;
+  transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
+  border-radius: 200px;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  width: 100%;
+  background-color: rgba(255, 255, 255, 0.1);
+  box-shadow: 0 2px 80px 0 rgba(255, 255, 255, 0.2);
+  transition: all 200ms ease-in 0s;
 `;
 
-const InputWrapper = styled.div``;
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2rem;
+  border-radius: 30px;
+`;
+
+const InputFieldDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  @media (max-width: 780px) {
+    margin-left: 0.8rem;
+    margin-right: 0.8rem;
+  }
+  margin-left: 1rem;
+  margin-right: 1rem;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.1);
+  box-shadow: 0 2px 30px 0 rgba(255, 255, 255, 0.2);
+
+  :hover {
+    border: 1px solid #b8d0d5;
+    opacity: 0.8;
+    background-color: rgba(255, 255, 255, 0.2);
+    cursor: ns-resize;
+  }
+`;
+
+const InputField = styled.input`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  text-align: center;
+  @media (max-width: 780px) {
+    padding-left: 0px;
+    height: 50px;
+    width: 3rem;
+  }
+  padding-left: 14px;
+  height: 60px;
+  width: 5rem;
+  font-size: 1.1rem;
+  color: rgba(255, 255, 255, 0.8);
+  opacity: 0.8;
+  transition: all 200ms ease-in 0s;
+`;
+
+const ScrollIcon = styled(CgScrollV)`
+  height: 30px;
+  width: 30px;
+  color: rgba(255, 255, 255, 0.8);
+  opacity: 0.8;
+  cursor: pointer;
+`;
 
 const Heading = styled.div`
-  margin: 30px;
+  margin: 20px;
+  text-align: center;
+  padding: 15px;
+  font-size: 1.1rem;
+  border-radius: 30px;
+  color: rgba(255, 255, 255, 0.8);
+  opacity: 0.8;
+
+  p {
+    margin-bottom: 8px;
+  }
+`;
+
+const LoginButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background-color: rgba(255, 255, 255, 0.1);
+  box-shadow: 0 2px 30px 0 rgba(255, 255, 255, 0.2);
+  @media (max-width: 780px) {
+    width: 6rem;
+  }
+  height: 50px;
+  width: 7rem;
+  font-size: 1.2rem;
+  color: rgba(255, 255, 255, 0.8);
+  opacity: 0.8;
+  border-radius: 30px;
+  transition: all 200ms ease-in 0s;
+
+  :hover {
+    border: 1px solid #b8d0d5;
+    opacity: 0.8;
+    background-color: rgba(255, 255, 255, 0.2);
+    cursor: pointer;
+  }
 `;
 
 export default MainPage;
